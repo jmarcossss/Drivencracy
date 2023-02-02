de6e736afae0cb1777c3b90ccc26a93cd3b4f8b5
jmarcossss <joaomalcantaraaa@gmail.com>
2023-02-02 12:02:46 -0300

    feat:rota enquete

---
+++ src/controllers/controllerVote.js
@@ -0,0 +1,45 @@
+import dayjs from "dayjs";
+import { ObjectId } from "mongodb";
+import { databasePoll } from "../database/dbs.js";
+import { databaseChoice } from "../database/dbs.js";
+import { databaseVoto } from "../database/dbs.js";
+
+export async function postVote(require, response) {
+  const { id } = require.params;
+  try {
+    //Faz a busca do choice
+    let waitDbChoice = await databaseChoice.findOne({ _id: new ObjectId(id) });
+    if(!waitDbChoice) {
+      //Se não encontrar o choice, dá erro 404
+      return response.status(404).send("Não encontrado");
+    }
+    await databaseVoto.insertOne({
+      createAt: dayjs().format("YYYY-MM-DD HH:mm"),
+      choiceId: new ObjectId(id),
+    });
+    //Se tiver encontrado, imcrementa o voto em 1 unidade, status 201
+    response.status(201).send("Voto registrado com sucesso");
+  }
+  catch(err) {
+    console.error(err);
+    response.sendStatus(500);
+  }
+}
+
+export async function getVote(require, response) {
+  const { id } = require.params;
+  try {
+    let poll = await databasePoll.findOne({ _id: new ObjectId(id) });
+    if(!poll) {
+      return response.status(404).send("Enquete não encontrada");
+    }
+    const fullChoices = await databaseChoice.find({ pollId: new ObjectId(id) }).toArray();
+    const winner = countVote(fullChoices);  
+    let dibParam = {...poll, responseult: { ...winner }, };
+    response.send(dibParam);
+  }
+  catch(err) {
+    console.error(err);
+    response.sendStatus(500);
+  }
+}
--- src/rotas/rotaEnquete.js
+++ src/rotas/rotaEnquete.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getVote, postVote } from "../controllers/vote.controller.js";
+import { getVote, postVote } from "../controllers/controllerVote.js";
 
 const rotaEnquete = express.Router();
 
 //Registro de votos
 rotaEnquete.post("/choice/:id/vote", postVote);
-//Retorna a opção e a quantidade de votos
+//Retorna a opção e a quantidade de votos mais votados
 rotaEnquete.get("/poll/:id/result", getVote);
 
 export default rotaEnquete;
